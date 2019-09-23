154ad76dec9b500ac8d345fe8c763ff7ea3a433e
Matthew Mattox <cube8021@gmail.com>
2019-09-23 00:04:29 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 echo "Starting auto build process..."
 
 cd ./server
-docker build -t registry.support.tools/supportcollect/server:"$GIT_BRANCH":"$BUILD_NUMBER" .
-docker push registry.support.tools/supportcollect/server:"$GIT_BRANCH":"$BUILD_NUMBER"
+docker build -t registry.support.tools/supportcollect/server:"${GIT_BRANCH#*/}":"$BUILD_NUMBER" .
+docker push registry.support.tools/supportcollect/server:"${GIT_BRANCH#*/}":"$BUILD_NUMBER"
 cd ..
