bcc74ce3fa1ef227608cf398c4c61eadd3d6198f
Matthew Mattox <mmattox@support.tools>
2019-09-24 16:58:19 -0500

    added deploy files and moved worker

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Starting auto build process..."
-
-cd ./server
-docker build -t registry.support.tools/supportcollect/server:"${GIT_BRANCH#*/}"-"$BUILD_NUMBER" .
-docker push registry.support.tools/supportcollect/server:"${GIT_BRANCH#*/}"-"$BUILD_NUMBER"
-cd ..
